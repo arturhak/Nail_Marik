5c69aa9775c97abccc5d1096e0d79ae109270ba7
arturhak <arturhak08@gmail.com>
2024-04-09 19:30:22 +0400

    add-footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from "./auth/Header";
+import Footer from "./auth/Footer";
 
 function App() {
-  return (
-    <div className="nail-app">
-      "NAIL_APP"
-    </div>
-  );
+    return (
+        <>
+           <div className="App">
+               <Header/>
+               <div style={{height:"9500px",background: "rebeccapurple",overflow: "auto"}}></div>
+               <Footer/>
+           </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/auth/Footer.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Logo from "../assets/logo.svg";
+import logoInstagram from "../assets/instagram.svg";
+import logoFacebook from "../assets/facebook.svg";
+import MainButton from "../MainButton";
+
+function Footer () {
+    return (
+        <div className="footer" >
+            <div className="logo">
+                <img src={Logo} alt="logo"/>
+            </div>
+            <div className="contacts">
+                <div className="contacts_item">
+                    Step into a Realm of Beauty and Elegance, Where Your Nails Become a Canvas of Creativity.
+                </div>
+                <div className="contacts_item">
+                    Vardananc 16, Yerevan, Armenia
+                </div>
+                <div className="contacts_item">
+                    (374-10)229622
+                </div>
+            </div>
+            <div className="social-links">
+                <img src={logoInstagram} alt="instagram" rel="nofollow"/>
+                <img src={logoFacebook} alt="facebook" rel="nofollow"/>
+            </div>
+            <MainButton />
+        </div>
+    )
+}
+
+export default Footer
